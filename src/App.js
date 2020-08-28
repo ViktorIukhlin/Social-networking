767842ec10bf057ff020a9e0b4dbbfbcf0a1b1bf
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-08-28 11:27:56 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>REACT</li>
-      </ul>
+    <div className="app-wrapper">
+        <Header />
+        
+      <nav className="nav">
+          <div>Profile</div>
+          <div>Messages</div>
+          <div>News</div>
+          <div>Music</div>
+          <div>Settings</div>
+      </nav>
+
+      <main className="main">
+        <div>
+                    <img className="picture-up" src="https://i.pinimg.com/originals/29/b1/f8/29b1f86e4e3ded955528cbc4ba548d48.jpg" alt="gras"/>
+
+          <div className="portfolio">
+            <img className ="photoMy" src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" alt="PhotoMy"/>
+            <div className="about">
+              <div>Batton B.</div>
+
+              <div>Date of Brith: 2 january</div>
+              <div>City: Saint-Petersburg</div>
+              <div>Education: BSU'11</div>
+              <div>Web Site: https://batton-batonovich.com</div>
+            </div>
+
+            <div className="posts">
+              My posts
+            </div>
+            <div className="posts">
+              My posts
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
+
   ) 
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src ='https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Internet_Defense_League_logo_-_cat_face.svg/1200px-Internet_Defense_League_logo_-_cat_face.svg.png' alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
