a806e03fb84cef1d1cf357c3766321d8abe5e834
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-01 23:08:32 +0300

    add activeclass and ctyle for messangers

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import s from './Messages.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Messages = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.namesList}>
-                <li className={s.name +' '+ s.active}>Hanna</li>
-                <li className={s.name}>Boris</li>
-                <li className={s.name}>Alex</li>
-                <li className={s.name}>BonBon</li>
-                <li className={s.name}>Marina</li>
-                <li className={s.name}>Leva</li>
+                <div className={s.nameContainer}>
+                    <NavLink to="/messages/1" className={s.name} activeClassName={s.active}>Hanna</NavLink>
+                </div>
+                <div  className={s.nameContainer}>
+                    <NavLink to="/messages/2" className={s.name} activeClassName={s.active}>BonBon</NavLink>
+                </div>
+                <div  className={s.nameContainer}>
+                    <NavLink to="/messages/3" className={s.name} activeClassName={s.active}>Valera</NavLink>
+                </div>
+                <div  className={s.nameContainer}>
+                    <NavLink to="/messages/4" className={s.name} activeClassName={s.active}>Tim</NavLink>
+                </div>
+                <div  className={s.nameContainer}>
+                    <NavLink to="/messages/5" className={s.name} activeClassName={s.active}>Mike</NavLink>
+                </div>
+                <div  className={s.nameContainer}>
+                    <NavLink to="/messages/6" className={s.name} activeClassName={s.active}>Boris</NavLink>
+                </div>
             </div>
             <div className={s.content}>
                 <p className={s.message}>I think the time has come...</p>
--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => {
     return (
         <nav className={s.nav}>
-            <NavLink className={s.link} activeClassName={s.active} to="./profile"> Profile </NavLink>
-            <NavLink className={s.link} activeClassName={s.active} to="./messages"> Messages </NavLink>
-            <NavLink className={s.link} activeClassName={s.active} to="./news"> News </NavLink>
-            <NavLink className={s.link} activeClassName={s.active} to="./music"> Music </NavLink>
-            <NavLink className={s.link} activeClassName={s.active} to="./settings"> Settings </NavLink>
+            <NavLink className={s.link} activeClassName={s.active} to="/profile"> Profile </NavLink>
+            <NavLink className={s.link} activeClassName={s.active} to="/messages"> Messages </NavLink>
+            <NavLink className={s.link} activeClassName={s.active} to="/news"> News </NavLink>
+            <NavLink className={s.link} activeClassName={s.active} to="/music"> Music </NavLink>
+            <NavLink className={s.link} activeClassName={s.active} to="/settings"> Settings </NavLink>
         </nav>
     )
 }
