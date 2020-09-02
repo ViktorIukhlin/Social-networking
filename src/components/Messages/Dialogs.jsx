c641a432d4954dcf5833d6b6be8189dcd115dd99
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-02 18:16:21 +0300

    add arr + map messages+ dialogs

--- src/components/Messages/Dialogs.jsx
+++ src/components/Messages/Dialogs.jsx
@@ -2,21 +2,40 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 
+const Message = (props) => {
+    return (
+    <div className={s.message}>{props.message}</div>
+    )
+}
+let messages = [
+    {id: 1, message: 'I think the time has come...'},
+    {id: 1, message: 'I\'m very angry'},
+    {id: 1, message: 'I know you ate my food!!!'}
+]
+let dialogsMessages = messages.map(
+    item => <Message message={item.message} />
+)
+
+let dialogs = [
+   { name: 'Hanna', id: 1},
+   { name: 'BonBon', id: 2},
+   { name: 'Mike', id: 3},
+   { name: 'Dino', id: 4},
+   { name: 'Lex', id: 5},
+   { name: 'MoMo', id: 6}
+]
+let dialogsItems = dialogs.map(
+    item => <DialogItem name={item.name} id={item.id} />
+)
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.namesList}>
-                <DialogItem name="Hanna" id="1"/>
-                <DialogItem name="BonBon" id="2"/>
-                <DialogItem name="Mike" id="3"/>
-                <DialogItem name="Dino" id="4"/>
-                <DialogItem name="Lex" id="5"/>
-                <DialogItem name="MoMo" id="6"/>
+                {dialogsItems}
             </div>
             <div className={s.content}>
-                <p className={s.message}>I think the time has come...</p>
-                <p className={s.message}>I'm very angry</p>
-                <p className={s.message}>I know you ate my food!!!</p>
+                {dialogsMessages}
             </div>
         </div>
     )
