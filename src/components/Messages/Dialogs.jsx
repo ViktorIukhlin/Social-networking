81aefcd1b43cf58c9517dede458011fcbc75b587
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-02 21:13:24 +0300

    add first protototype of database

--- src/components/Messages/Dialogs.jsx
+++ src/components/Messages/Dialogs.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
+import messages from './datebaseMessages';
+import dialogs from './databaseDialogs';
 
-const Message = (props) => {
-    return (
-    <div className={s.message}>{props.message}</div>
-    )
-}
-let messages = [
-    {id: 1, message: 'I think the time has come...'},
-    {id: 1, message: 'I\'m very angry'},
-    {id: 1, message: 'I know you ate my food!!!'}
-]
 let dialogsMessages = messages.map(
     item => <Message message={item.message} />
 )
-
-let dialogs = [
-   { name: 'Hanna', id: 1},
-   { name: 'BonBon', id: 2},
-   { name: 'Mike', id: 3},
-   { name: 'Dino', id: 4},
-   { name: 'Lex', id: 5},
-   { name: 'MoMo', id: 6}
-]
 let dialogsItems = dialogs.map(
     item => <DialogItem name={item.name} id={item.id} />
 )
---
+++ src/components/Messages/datebaseMessages.jsx
@@ -0,0 +1,7 @@
+let messages = [
+    {id: 1, message: 'I think the time has come...'},
+    {id: 1, message: 'I\'m very angry'},
+    {id: 1, message: 'I know you ate my food!!!'}
+]
+
+export default messages;
