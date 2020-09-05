b9e1a9c5a6f71e8b509f10f56cd1d354dbbd7c3f
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-05 18:21:47 +0300

    Updates

---
+++ precache-manifest.a9f96e89de6b42123bfd418f5e571c8c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8daae5c2699efeb94d23f57dd4723b70",
+    "url": "/Social-networking/index.html"
+  },
+  {
+    "revision": "3af0570714d3310ea3c0",
+    "url": "/Social-networking/static/css/main.0c30cfaf.chunk.css"
+  },
+  {
+    "revision": "28eeca04f60dd379c158",
+    "url": "/Social-networking/static/js/2.41d6d08a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-networking/static/js/2.41d6d08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af0570714d3310ea3c0",
+    "url": "/Social-networking/static/js/main.f0886a53.chunk.js"
+  },
+  {
+    "revision": "f20a57fc55a43efc73a3",
+    "url": "/Social-networking/static/js/runtime-main.84785785.js"
+  },
+  {
+    "revision": "725bc066722fdfc327f582568e8ce97e",
+    "url": "/Social-networking/static/media/Beon.725bc066.svg"
+  },
+  {
+    "revision": "ab8767c852a31db93db91e271b7ff538",
+    "url": "/Social-networking/static/media/Beon.ab8767c8.ttf"
+  },
+  {
+    "revision": "c78ba3cfb71280b8f36bccc844667ff0",
+    "url": "/Social-networking/static/media/Beon.c78ba3cf.woff"
+  },
+  {
+    "revision": "e1b2715c92d9f70f719bb04857e04ae4",
+    "url": "/Social-networking/static/media/Beon.e1b2715c.eot"
+  },
+  {
+    "revision": "6be7e62a93ee231eed1c82ffa9f73a15",
+    "url": "/Social-networking/static/media/Neon.6be7e62a.woff"
+  },
+  {
+    "revision": "75693c8da9edb55e658afb293a6d0d48",
+    "url": "/Social-networking/static/media/Neon.75693c8d.ttf"
+  },
+  {
+    "revision": "b7453472ffec35c27f6ef28db6089e34",
+    "url": "/Social-networking/static/media/Neon.b7453472.svg"
+  },
+  {
+    "revision": "d2a38f18725fc9ad7e02d13209c80398",
+    "url": "/Social-networking/static/media/Neon.d2a38f18.eot"
+  },
+  {
+    "revision": "05c2c5b942534249c4dc852d6ce768c3",
+    "url": "/Social-networking/static/media/TextMeOne-Regular.05c2c5b9.svg"
+  },
+  {
+    "revision": "475b539094d914ff68e420b4571c96c3",
+    "url": "/Social-networking/static/media/TextMeOne-Regular.475b5390.ttf"
+  },
+  {
+    "revision": "4a74a87e1fd70a04270572c36e0213e0",
+    "url": "/Social-networking/static/media/TextMeOne-Regular.4a74a87e.eot"
+  },
+  {
+    "revision": "ddd4573d95bb72bc585be0f4c03b69e7",
+    "url": "/Social-networking/static/media/TextMeOne-Regular.ddd4573d.woff"
+  }
+]);
