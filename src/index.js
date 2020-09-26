71a5b6e2fa7a81d7e1f55abeadce64264a3ed2c4
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-26 17:52:46 +0300

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as serviceWorker from './serviceWorker';
-import store from './redux/state';
+import store from './redux/store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
