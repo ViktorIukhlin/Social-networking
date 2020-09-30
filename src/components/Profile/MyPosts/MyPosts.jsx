30a91e68fa26818caaa185c0f8e937990f950ad3
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-30 20:40:13 +0300

    ref+

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,38 +3,37 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-    let myPosts = props.posts.map(
-        item => <Post id={item.id} message={item.message} key={item.id} likesCount={item.likesCount} />
-    )
+  let myPosts = props.posts.map(
+    item => <Post id={item.id} message={item.message} key={item.id} likesCount={item.likesCount} />
+  )
 
-    let newPostElement = React.createRef();
+  let newPostElement = React.createRef();
 
-    const onAddPost = () => {
-        props.addPost();
-    }
+  const onAddPost = () => {
+    props.addPost();
+  }
 
-    const onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
-    }
+  const onPostChange = () => {
+    let text = newPostElement.current.value;
+    props.updateNewPostText(text);
+  }
 
-    return (
-        <div>
-            My Post
-            <div className={s.area}>
-                <textarea onChange={onPostChange} 
-                ref={newPostElement} 
-                value={props.newPostText}
-                /> 
+  return (
+    <div>
+      My Post
+      <div className={s.area}>
+        <textarea onChange={onPostChange} 
+        ref={newPostElement} 
+        value={props.newPostText}
+        /> 
 
-                <button onClick={onAddPost}>Send</button>
-            </div>
-            <div className={s.posts}>
-                {myPosts}
-            </div>
-        </div>
-
-    )
+        <button onClick={onAddPost}>Send</button>
+      </div>
+      <div className={s.posts}>
+        {myPosts}
+      </div>
+    </div>
+  )
 }
 
 
