2fdd66a61d07a436ae990daab5aaa92f3025ab22
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-10-12 15:38:48 +0300

    create preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followActionCreator, setUsersActionCreator, unfollowActionCreator, setCurrentPageAC, setUsersTotalCountAC } from '../../redux/usersReducer';
+import { followActionCreator, setUsersActionCreator, unfollowActionCreator, setCurrentPageAC, setUsersTotalCountAC, toggleIsFetchingAC } from '../../redux/usersReducer';
 import Users from './Users';
-import * as axios from "axios"
+import * as axios from 'axios'
+import Preloader from '../common/Preloader/Preloader';
 
 class UsersContainer extends React.Component {
 
   componentDidMount(){
+    this.props.toggleIsFetching(true)
     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
     .then(response => {
+      this.props.toggleIsFetching(false)
       this.props.setUsers(response.data.items);
       this.props.setTotalUsersCount(response.data.totalCount);
     });
   }
 
   onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
+    this.props.setCurrentPage(pageNumber)
+    this.props.toggleIsFetching(true)
     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
     .then(response => {
+      this.props.toggleIsFetching(false)
       this.props.setUsers(response.data.items)
     });
   }
 
   render() {
-    return <Users 
+    return <>
+    {this.props.isFetching ? <Preloader /> : null}
+    <Users 
     totalUsersCount={this.props.totalUsersCount} 
     currentPage={this.props.currentPage}
     pageSize={this.props.pageSize}
@@ -31,7 +38,8 @@ class UsersContainer extends React.Component {
     users={this.props.users}
     follow={this.props.follow}
     unfollow={this.props.unfollow}
-    />;
+    />
+    </>
   }
 }
 
@@ -40,7 +48,8 @@ let mapStateToProps = (state) => {
     users: state.usersPage.users,
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage
+    currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching
   }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -59,6 +68,9 @@ let mapDispatchToProps = (dispatch) => {
     },
     setTotalUsersCount: (totalCount) => {
       dispatch(setUsersTotalCountAC(totalCount));
+    },
+    toggleIsFetching: (isFetching) =>{
+      dispatch(toggleIsFetchingAC(isFetching));
     }
   }
 }
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+let Preloader = (props) => {
+  return <img src={require("../../../img/spinner.svg")} alt="spiner"/> 
+}
+
+export default Preloader;
