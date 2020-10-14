0f24429cff2a71194becabe317656653e359d6df
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-10-14 21:24:39 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = (props) => {
         <Nav state={props.store}/>
         <div className="app-wrapper-content">
           <Route path='/messages' render={ () => <DialogsContainer />} />
-          <Route path='/profile'  render={ () => <ProfileContainer />}/>
+          <Route path='/profile/:userId?'  render={ () => <ProfileContainer />}/>
           <Route path='/users'  render={ () => <UsersContainer />}/>
 
           <Route path='/news' component={News} />
