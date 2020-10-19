9d062a0a09168c03d91ec073ced036fb29ece53c
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-10-19 17:18:19 +0300

    ++

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -24,11 +24,11 @@ class ProfileStatus extends React.Component {
         {!this.state.editMode 
         ?
           <div>
-            <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
+            <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
           </div>
         :
           <div>
-            <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.props.status} />
+            <input autoFocus={true} onBlur={ this.deactivateEditMode } value={this.props.status} />
           </div>
         } 
       </div>
